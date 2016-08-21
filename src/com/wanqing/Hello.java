97faf11a24ca17036de43fe2bfe611dcf00f9496
wq3100100985 <wq3100100985@gmail.com>
2016-08-21 16:59:45 +0800

    Add a line

--- src/com/wanqing/Hello.java
+++ src/com/wanqing/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello java");
+		System.out.println("hello git");
 	}
 	
 	public int add(int a,int b){
